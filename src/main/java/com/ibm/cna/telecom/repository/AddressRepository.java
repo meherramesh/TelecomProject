e2fa3319493f9d075e2ef5a8eb1ee21fed989e07
Code_Debugger_Ramesh <115032628+meherramesh@users.noreply.github.com>
2024-01-29 17:17:26 +0530

    Update AddressRepository.java

--- src/main/java/com/ibm/cna/telecom/repository/AddressRepository.java
+++ src/main/java/com/ibm/cna/telecom/repository/AddressRepository.java
@@ -1,16 +1,10 @@
 package com.ibm.cna.telecom.repository;
-
 import java.io.Serializable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ibm.cna.telecom.model.Address;
 
-
-/**
-*
-* @author Chetana
-*/
 @Repository
 public interface AddressRepository extends CrudRepository<Address, Serializable>{
 	
