73d356bef751a392bae54c95efe56c88065d1166
Code_Debugger_Ramesh <115032628+meherramesh@users.noreply.github.com>
2024-01-29 17:12:11 +0530

    Update DataConnection.java

--- src/main/java/com/ibm/cna/telecom/factory/DataConnection.java
+++ src/main/java/com/ibm/cna/telecom/factory/DataConnection.java
@@ -1,21 +1,12 @@
 package com.ibm.cna.telecom.factory;
 
-
 import com.ibm.cna.telecom.service.ConnectionTypeService;
-/**
- * @author Chetana
- */
+
 public class DataConnection implements ConnectionTypeService{
-
-
 
 	@Override
 	public String getConnectionType() {
 		// TODO Auto-generated method stub
 		return "Data Connection";
-	}
-
-	
-
-	
+	}	
 }
