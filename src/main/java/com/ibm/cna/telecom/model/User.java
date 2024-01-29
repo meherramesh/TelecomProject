569d18891dcfc45c76ce1e0fc00807644927568e
Code_Debugger_Ramesh <115032628+meherramesh@users.noreply.github.com>
2024-01-29 17:16:07 +0530

    Update User.java

--- src/main/java/com/ibm/cna/telecom/model/User.java
+++ src/main/java/com/ibm/cna/telecom/model/User.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ibm.cna.telecom.model;
 
 import java.io.Serializable;
@@ -13,10 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- *
- * @author Chetana
- */
 @Entity
 @Table(name = "user")
 public class User implements Serializable {
