3fa5040d63af507a66c006e78aeff880daf197bc
Code_Debugger_Ramesh <115032628+meherramesh@users.noreply.github.com>
2024-01-29 17:19:27 +0530

    Update EmployeeService.java

--- src/main/java/com/ibm/cna/telecom/service/EmployeeService.java
+++ src/main/java/com/ibm/cna/telecom/service/EmployeeService.java
@@ -2,13 +2,8 @@ package com.ibm.cna.telecom.service;
 
 import com.ibm.cna.telecom.model.Employee;
 
-/**
- *
- * @author Chetana
- */
 public interface EmployeeService {
 
 	public Employee saveEmployee(Employee employee);
-
 
 }
