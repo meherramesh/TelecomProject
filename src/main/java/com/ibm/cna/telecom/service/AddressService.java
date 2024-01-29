93e61580ce13c4ec1a1bffce6222d8c4a742c3cd
Code_Debugger_Ramesh <115032628+meherramesh@users.noreply.github.com>
2024-01-29 17:18:49 +0530

    Update AddressService.java

--- src/main/java/com/ibm/cna/telecom/service/AddressService.java
+++ src/main/java/com/ibm/cna/telecom/service/AddressService.java
@@ -2,12 +2,7 @@ package com.ibm.cna.telecom.service;
 
 import com.ibm.cna.telecom.model.Address;
 
-/**
-*
-* @author Chetana
-*/
 public interface AddressService {
 	
-	public Address saveAddress(Address address);
-	
+	public Address saveAddress(Address address);	
 }
